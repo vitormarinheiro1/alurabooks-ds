068b551cfc73d7542335c0597adb2ae07696c20f
vitormarinheiro1 <vitormarinheiro9@gmail.com>
2024-07-17 15:55:47 -0300

    fix: package.json

--- src/index.tsx
+++ src/index.tsx
@@ -14,5 +14,6 @@ export const Thing: FC<Props> = ({ children }) => {
   return <div>{children || `the snozzberries taste like snozzberries`}</div>;
 };
 
+export * from './componentes/AbCard'
 export * from './componentes/AbBotao'
 export * from './componentes/AbGrupoOpcoes'
